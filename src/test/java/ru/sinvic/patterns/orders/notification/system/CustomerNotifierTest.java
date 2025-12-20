57e2a1caf51550063cd40d608046207c6343e548
Victoria <vicktoria.sin@yandex.ru>
2025-12-21 00:25:51 +0300

    add test to customer notifier

--- src/test/java/ru/sinvic/patterns/orders/notification/system/CustomerNotifierTest.java
+++ src/test/java/ru/sinvic/patterns/orders/notification/system/CustomerNotifierTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,54 +14,80 @@ import ru.sinvic.patterns.orders.notification.system.service.MessengerService;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerNotifierTest {
-    @Captor
-    private ArgumentCaptor<String> chatNameCaptor;
-    @Captor
-    private ArgumentCaptor<String> textCaptor;
-    @Captor
-    private ArgumentCaptor<String> emailCaptor;
-
-
+    @Mock
+    private EmailService emailService;
 
     @Mock
-    EmailService emailService;
+    private MessengerService messengerService;
 
-    @Mock
-    MessengerService messengerService;
+    @Captor
+    private ArgumentCaptor<String> messengerChatCaptor;
 
-    CustomerNotifier customerNotifier;
+    @Captor
+    private ArgumentCaptor<String> messengerTextCaptor;
 
-    List<String> emails = List.of("a@yandex.ru", "b@yandex.ru");
-    Order order = new Order(1L, "Order", LocalDateTime.now());
-    String messageText = STR."Order \{order.id()} has changed";
-    String MESSENGER_CHAT = "orders_updates";
+    @Captor
+    private ArgumentCaptor<String> emailRecipientCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> emailSubjectCaptor;
+
+    private CustomerNotifier customerNotifier;
+
+    private final List<String> emails = List.of("a@yandex.ru", "b@yandex.ru");
+    private final Order order = new Order(1L, "Order", LocalDateTime.now());
+    String text = STR."Order \{order.id()} has changed";
 
     @BeforeEach
     void setUp() {
         customerNotifier = new CustomerNotifier(emailService, messengerService, emails);
     }
 
+
     @Test
-    void shouldSendEmailsAndMessagesAfterNotificationHappens() {
+    void shouldSendSingleMessengerNotificationWithCorrectArgs() {
         customerNotifier.update(order);
 
-        verify(messengerService, times(1)).sendMessage(ArgumentMatchers.any(), ArgumentMatchers.any());
-        verify(emailService, times(2)).sendEmail(ArgumentMatchers.any(), ArgumentMatchers.any());
+        verify(messengerService).sendMessage(
+            messengerChatCaptor.capture(),
+            messengerTextCaptor.capture()
+        );
 
+        List<String> chatNames = messengerChatCaptor.getAllValues();
+        List<String> texts = messengerTextCaptor.getAllValues();
 
-        verify(messengerService, times(1))
-            .sendMessage(chatNameCaptor.capture(), textCaptor.capture());
+        assertEquals(1, chatNames.size());
+        String expectedChatName = "orders_updates";
+        assertEquals(expectedChatName, chatNames.getFirst(), "Неверный чат для уведомления");
+        assertEquals(text, texts.get(0), "Неверный текст сообщения");
+    }
 
-        List<String> capturedChatNames = chatNameCaptor.getAllValues();
-        List<String> capturedTexts = textCaptor.getAllValues();
-        assertEquals(MESSENGER_CHAT, capturedChatNames.get(0));
-        assertEquals(messageText, capturedTexts.get(0));
+    @Test
+    void shouldSendEmailsToAllSubscribedAddresses() {
+        customerNotifier.update(order);
 
+        verify(emailService, times(2)).sendEmail(
+            emailRecipientCaptor.capture(),
+            emailSubjectCaptor.capture()
+        );
+
+        List<String> recipients = emailRecipientCaptor.getAllValues();
+        assertEquals(emails, recipients, "Письма должны быть отправлены на все указанные адреса");
+
+        List<String> subjects = emailSubjectCaptor.getAllValues();
+        assertTrue(subjects.stream().allMatch(s -> s.contains(text)));
+    }
+
+    @Test
+    void shouldNotPerformOtherServiceCalls() {
+        customerNotifier.update(order);
+
+        verifyNoMoreInteractions(emailService, messengerService);
     }
 }

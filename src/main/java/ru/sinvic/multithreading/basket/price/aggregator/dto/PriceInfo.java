2a3352681f1216cfa455b8d1adf01a9be1b4a328
Victoria <vicktoria.sin@yandex.ru>
2025-12-25 23:52:21 +0300

    add checking of base price to PriceInfo

--- src/main/java/ru/sinvic/multithreading/basket/price/aggregator/dto/PriceInfo.java
+++ src/main/java/ru/sinvic/multithreading/basket/price/aggregator/dto/PriceInfo.java
@@ -9,11 +9,22 @@ import java.util.List;
 
 public record PriceInfo(long productId, @NonNull BigDecimal basePrice, @NonNull List<Promo> promoTypes) {
     public PriceInfo {
+        checkCashBackPromoQuantity();
+        checkBasePrice();
+    }
+
+    private void checkCashBackPromoQuantity() {
         long cashBackPromoCount = promoTypes.stream()
             .filter(promo -> promo instanceof PromoCashBack)
             .count();
         if (cashBackPromoCount > 1) {
             throw new IllegalArgumentException("There can only be one cash back promo!");
+        }
+    }
+
+    private void checkBasePrice() {
+        if (basePrice.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException(STR."Base price can not be less than or equal to zero! Get: \{basePrice}");
         }
     }
 }
